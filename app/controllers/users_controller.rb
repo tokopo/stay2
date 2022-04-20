53a04b9684da44f074308b5addd4c8b85a699a11
tokopo <94882217+tokopo@users.noreply.github.com>
2022-04-20 15:40:45 +0900

    show profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,5 @@ class UsersController < ApplicationController
   def account
     @user = current_user
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   root to: 'home#top'
   resources :rooms
-  get 'users/account'
-  get 'users/profile'
+  namespace :users do
+    resource :profile, only: [:show, :update]
+    resource :account, only: :show
+  end
 
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
